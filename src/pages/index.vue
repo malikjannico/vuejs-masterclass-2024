893c2a0e1e45669654e0da60b2668198635ecc80
malikjannico <malik4privat@gmail.com>
2025-08-13 16:36:01 +0200

    update

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Got to Project 1</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
