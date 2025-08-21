1d29403befe4ebd7808c764592984cac019ec288
malikjannico <malik4privat@gmail.com>
2025-08-21 15:37:59 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { error } from 'console'
 
 const app = createApp(App)
 
--- src/utils/supaQueries.ts
+++ src/utils/supaQueries.ts
@@ -37,3 +37,7 @@ export type Project = QueryData<ReturnType<typeof projectQuery>>
 export const taskQuery = (id: number) =>
   supabase.from('tasks').select(`*, projects(id, name, slug)`).eq('id', id).single()
 export type Task = QueryData<ReturnType<typeof taskQuery>>
+
+export const profileQuery = (id: string) => {
+  return supabase.from('profiles').select().eq('id', id).single
+}
