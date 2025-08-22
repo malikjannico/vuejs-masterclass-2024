2ef322231eb6fdd0220e8de784bdf56b4889d296
malikjannico <malik4privat@gmail.com>
2025-08-22 14:53:36 +0200

    update

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -4,18 +4,12 @@
 
 <script setup lang="ts">
 import { columns } from '@/utils/tableColumns/projectColumns'
-import { projectsQuery, type Projects } from '@/utils/supaQueries'
-import { useErrorStore } from '@/stores/error'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Projects | null>(null)
-
-const getProjects = async () => {
-  const { data, error, status } = await projectsQuery
-  if (error) useErrorStore().setError({ error, customCode: status })
-  projects.value = data
-}
+const projectsLoader = useProjectsStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 
 await getProjects()
 </script>
