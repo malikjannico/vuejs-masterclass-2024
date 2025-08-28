2737e29afafdb9b769438aecf553e38fd9e84414
malikjannico <malik4privat@gmail.com>
2025-08-28 15:57:01 +0200

    update

--- src/components/appError/AppErrorPage.vue
+++ src/components/appError/AppErrorPage.vue
@@ -50,8 +50,7 @@ router.afterEach(() => {
   </section>
 </template>
 
-<style scoped>
-@reference "@/assets/index.css";
+<style scoped lang="postcss">
 .error {
   @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
 }
