32752554f59780a1930a8338af58590813377c3f
malikjannico <malik4privat@gmail.com>
2025-08-28 15:02:15 +0200

    update

--- src/components/appInPlaceEdit/AppInPlaceEditTextarea.vue
+++ src/components/appInPlaceEdit/AppInPlaceEditTextarea.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-const inputValue = defineModel<string>({ required: true })
+const inputValue = defineModel<string | null>({ required: true })
 
 const emit = defineEmits(['commit'])
 
