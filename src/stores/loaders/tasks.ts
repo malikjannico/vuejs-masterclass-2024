280233244f86010233e4e7c96d49512fe376699b
malikjannico <malik4privat@gmail.com>
2025-09-01 11:53:11 +0200

    update

--- src/stores/loaders/tasks.ts
+++ src/stores/loaders/tasks.ts
@@ -1,32 +1,36 @@
-import { taskQuery, tasksWithProjectsQuery, updateTaskQuery } from '@/utils/supaQueries'
+import {
+  deleteTaskQuery,
+  taskQuery,
+  tasksWithProjectsQuery,
+  updateTaskQuery,
+  type Task,
+  type TasksWithProjects,
+} from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
-import type { deleteTaskQuery, Task, TasksWithProjects } from '@/utils/supaQueries'
 
 export const useTasksStore = defineStore('tasks-store', () => {
   const tasks = ref<TasksWithProjects | null>(null)
   const task = ref<Task | null>(null)
-  const loadTasks = useMemoize(
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async (id: string) => await tasksWithProjectsQuery,
-  )
-  const loadTask = useMemoize(async (slug: string) => await taskQuery(slug))
+  const loadTasks = useMemoize(async () => await tasksWithProjectsQuery)
+  const loadTask = useMemoize(async (id: number) => {
+    return taskQuery(id)
+  })
 
   interface ValidateCacheParams {
-    ref: typeof tasks | typeof task
-    query: typeof tasksWithProjectsQuery | typeof taskQuery
+    ref: typeof task | typeof tasks
+    query: typeof taskQuery | typeof tasksWithProjectsQuery
     key: string
-    loaderFn: typeof loadTasks | typeof loadTask
+    loader: typeof loadTask | typeof loadTasks
   }
 
-  const validateCache = ({ ref, query, key, loaderFn }: ValidateCacheParams) => {
+  const validateCache = ({ ref, query, key, loader }: ValidateCacheParams) => {
     if (ref.value) {
-      const finalQuery = typeof query === 'function' ? query(key) : query
-
+      const finalQuery = typeof query === 'function' ? query(Number(key)) : query
       finalQuery.then(({ data, error }) => {
         if (JSON.stringify(ref.value) === JSON.stringify(data)) {
           return
         } else {
-          loaderFn.delete(key)
+          loader.delete(key)
           if (!error && data) ref.value = data
         }
       })
@@ -35,57 +39,46 @@ export const useTasksStore = defineStore('tasks-store', () => {
 
   const getTasks = async () => {
     tasks.value = null
-
-    const { data, error, status } = await loadTasks('tasks')
-
+    const { data, error, status } = await loadTasks()
     if (error) useErrorStore().setError({ error, customCode: status })
-
     if (data) tasks.value = data
-
     validateCache({
       ref: tasks,
       query: tasksWithProjectsQuery,
       key: 'tasks',
-      loaderFn: loadTasks,
+      loader: loadTasks,
     })
   }
 
   const getTask = async (id: string) => {
     task.value = null
-
-    const { data, error, status } = await loadTask(id)
-
+    const { data, error, status } = await loadTask(Number(id))
     if (error) useErrorStore().setError({ error, customCode: status })
-
     if (data) task.value = data
-
     validateCache({
       ref: task,
       query: taskQuery,
       key: id,
-      loaderFn: loadTask,
+      loader: loadTask,
     })
   }
 
   const updateTask = async () => {
     if (!task.value) return
-
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { projects, id, ...taskProperties } = task.value
-
-    await updateTaskQuery(taskProperties, task.value.id)
+    const { id, projects, ...taskProps } = task.value
+    await updateTaskQuery(taskProps, task.value.id)
   }
 
-  const deleteTask = async () => {
-    if (!task.value) return
-    await deleteTaskQuery(task.value.id)
+  const deleteTask = async (id: number) => {
+    await deleteTaskQuery(id)
   }
 
   return {
     tasks,
+    task,
     getTasks,
     getTask,
-    task,
     updateTask,
     deleteTask,
   }
